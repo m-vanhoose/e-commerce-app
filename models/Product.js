613bce3262735cbae4ff165903174e2a4cffa6ea
m-vanhoose <m-vanhoose@live.com>
2023-04-24 15:47:11 -0400

    completed get/post/put/delete routes

--- models/Product.js
+++ models/Product.js
@@ -10,7 +10,7 @@ class Product extends Model {}
 Product.init(
   {
     id: {
-      type: DataTypes.INTERGER,
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
@@ -27,7 +27,7 @@ Product.init(
       }
     },
     stock: {
-      type: DataTypes.INTERGER,
+      type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
       validate: {
--- models/index.js
+++ models/index.js
@@ -5,17 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category)
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+})
 // Categories have many Products
-Category.hasMany(Product)
+Category.hasMany(Product, {
+  foreignKey: "category_id"
+})
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  trough: {
-
-  }
+  through: ProductTag
 })
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product)
+Tag.belongsToMany(Product, {
+  through: ProductTag
+})
 
 module.exports = {
   Product,
